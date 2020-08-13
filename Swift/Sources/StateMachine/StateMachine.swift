0b2caf7767410a2647d218da902669b5745b4065
Christopher Fuller <christopher.fuller@gotinder.com>
2020-08-13 09:04:53 -0700

    Add Swift file

---
+++ Swift/Sources/StateMachine/StateMachine.swift
@@ -0,0 +1,9 @@
+//
+//  Created by Christopher Fuller on 12/21/19.
+//  Copyright © 2019 Tinder. All rights reserved.
+//
+
+// Based on Andy Matuschak's composable pattern for pure state machines with effects (draft v3):
+// https://gist.github.com/andymatuschak/d5f0a8730ad601bcccae97e8398e25b2
+
+import Foundation
